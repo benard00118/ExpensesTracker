a1a83dd83858020a3505c01b1d04664a3cfa9d82
Benard Karanja <benardopeter4@gmail.com>
2024-11-03 09:57:17 +0300

    production version

--- expense_tracker/expense_tracker/urls.py
+++ expense_tracker/expense_tracker/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# expense_tracker/urls.py (project-level URLs)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('core.urls')),  # Include core app URLs
+    path('accounts/', include('django.contrib.auth.urls')),  # Django auth URLs
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
